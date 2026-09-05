904f41d06d359917cc6229a4bc53a076a53dbab2
Heibaixx <seah_shi_ting@hotmail.com>
2026-09-06 01:55:19 +0800

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Lunch-Picker/',,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
